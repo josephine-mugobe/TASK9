de1a5cd3e159da3d80863994bc2228f9dc6b51cf
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-04-05 16:40:14 +0300

    Commit#2
    In the old version on line 4 i've change the value 0.0003 which i've initialized there and set it to 1.0/3600 in the NV on line 4.
    also in the old version on line 5 i've change the value 3660 which i've initialized and set it to 3600 in the NV on line 5

--- TASK9/exchangeRates/src/main/java/exchange/rate.java
+++ TASK9/exchangeRates/src/main/java/exchange/rate.java
@@ -1,8 +1,8 @@
 package exchange;
 
 public class rate {
-    static final double UGX_to_USD = 0.0003;
-    static final double USD_to_UGX = 3660;
+    static final double UGX_to_USD = 1.0/3600;
+    static final double USD_to_UGX = 3600;
 
 
 }
