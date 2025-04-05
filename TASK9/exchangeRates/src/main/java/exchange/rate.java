ae9cbe6195bddd6b65680769b428d35b5b58970e
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-04-05 16:43:08 +0300

    commit#1
    In the NV from line 7 to 17 i've create a static method with some parameter

--- TASK9/exchangeRates/src/main/java/exchange/rate.java
+++ TASK9/exchangeRates/src/main/java/exchange/rate.java
@@ -4,5 +4,15 @@ public class rate {
     static final double UGX_to_USD = 1.0/3600;
     static final double USD_to_UGX = 3600;
 
-
+    public static double displayRate(String from, String to) {
+        if (from.equals("UGX")  && to.equals("USD") ) {
+            return UGX_to_USD;
+        } else if (from.equals("USD")  && to.equals("UGX")) {
+            return USD_to_UGX;
+        }
+        else {
+            System.out.println("It doesn't fit this conversion");
+            return 0;
+        }
+    }
 }
